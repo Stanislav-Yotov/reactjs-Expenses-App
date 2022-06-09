91b3a3ab5ccef441d532bf50563e2cbe298a0fa5
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-06-09 07:26:19 +0300

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import '../ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+        <div>
+            <div>25.04.2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$125.79</div>
+            </div>
+        </div>
+
+    );
+}
+
+export default ExpenseItem;
