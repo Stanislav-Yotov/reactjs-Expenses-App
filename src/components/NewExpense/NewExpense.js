c1ae043d74514166c42caf7924721e469a8d7873
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-06-15 09:15:48 +0300

    added dynamic rendering of components

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -8,7 +8,6 @@ function NewExpense(props) {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-        console.log(expenseData);
         props.onAddExpense(expenseData);
     };
 
