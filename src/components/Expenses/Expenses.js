ca6c052d14f66adb807c468bd8687234410e4796
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-06-17 16:58:40 +0300

    added ExpenseList component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -15,21 +15,11 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear
     });
 
-    let expenseContent = <p>No Expenses found.</p>;
-    if (filteredExpenses.length > 0) {
-        expenseContent = filteredExpenses.map(expense => <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date} />
-        )
-    }
-
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {expenseContent}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
